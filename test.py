d4dc313f568e76f836dd91b1cca15da23cce1e22
Özer Öngüç <ozeronguc@192.168.1.13>
2022-06-05 16:53:19 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,36 @@
+#print("Baaşarı ile giriş yaptınız! Hoş geldiniz", "Özer " + "Öngüç!")
+
+
+#myName = "  Sertaç      "
+#print(myName.strip())
+
+
+username = input("İsminizi girin ")
+surname = input("Soy isminizi girin ")
+password = input("Şifrenizi oluşturun ")
+year = int(input("Yaşınızı girin "))
+
+if year >= 18:
+    print("Hesabınız başarı ile oluşturuldu! ")
+
+else:
+    print("Yaşınız hesap oluşturmak için yetmiyor.")
+
+kalan_hak = 3
+
+while kalan_hak > 0 and year >= 18:
+    name = input("İsminizi girin ")
+    pw = input("Şifrenizi girin ")
+    
+
+    if username == name and password == pw:
+        print("Baaşarı ile giriş yaptınız! Hoş geldiniz", username, surname+"!")
+        #print("Baaşarı ile giriş yaptınız! Hoş geldiniz", username, surname.strip(),"!")
+        break
+    
+    else:
+        kalan_hak = kalan_hak -1
+        print("Giriş başarısız. Kalan hakkınız: ", kalan_hak)
+
+    if kalan_hak == 0:
+        print("Giriş başarısız bloke edildiniz, kalan hakkınız:", kalan_hak)
---
+++ test23.py
@@ -0,0 +1,13 @@
+kilo = input("Kilonuzu girin ")
+boy = input("Boyunuzu girin ")
+
+vkh = kilo/(boy*boy)
+
+if vkh <= 17:
+    print("Zayıfsın")
+
+elif vkh > 21:
+    print("sad")
+
+else:
+    print("yobez")
